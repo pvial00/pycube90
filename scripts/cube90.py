83968b934323b66bd82938e7312fa53c9b395435
Karl <pvial@krypt.local>
2017-08-30 14:16:28 -0700

    cube90 standalone script

--- scripts/cube90.py
+++ scripts/cube90.py
@@ -3,7 +3,7 @@ import sys, select, getpass, os
 
 if select.select([sys.stdin,],[],[],0.0)[0]:
     words = sys.stdin.read()
-    words = words.replace('\n', ' ')
+    words = words.replace('\n', '')
 else:
     words = raw_input("Enter text to cipher: ")
 
